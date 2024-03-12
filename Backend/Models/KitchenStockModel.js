58e6228e89cf448e732f1095ebe553e23339995c
Thiyasha0315 <theo.hfern@gmail.com>
2024-03-12 15:05:16 +0530

    model

---
+++ Backend/Models/KitchenStockModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const validator = require('validator')
+
+const stockSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: [true, "Please add a category"],
+      trim: true,
+    },
+    quantity: {
+      type: String,
+      required: [true, "Please add a quantity"],
+      trim: true,
+    },
+    price: {
+      type: String,
+      required: [true, "Please add a price"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Stock = mongoose.model("Stock", stockSchema);
+module.exports = Stock;
