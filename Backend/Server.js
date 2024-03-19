3e1f954251009838d0683b3a01aa36b9b4c28b21
Thiyasha0315 <theo.hfern@gmail.com>
2024-03-19 11:05:51 +0530

    ss

--- Backend/Server.js
+++ Backend/Server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 // routes
 
 app.use('/user', userRoutes)
-app.use('/kitchenStock', kicthenStockRoutes)
+app.use('/kitchenStock', kitchenStockRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGODB_URL)
