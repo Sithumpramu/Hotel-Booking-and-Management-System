8d783292656af1d0a4d6792ab90cc6569e993215
joanroche444 <joanroche1604@gmail.com>
2024-03-20 16:54:46 +0530

    route change

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const cors = require('cors');
 const mongoose = require('mongoose')
 const userRoutes = require('./Routes/user')
+const  hallRoutes  =require('./Routes/hallRoutes')
 
 // express app
 const app = express()
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 // routes
 
 app.use('/user', userRoutes)
+app.use('/hall',hallRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGODB_URL)
