b3fafd1297d52301c7e4f3f226c72ccd245e9c34
joanroche444 <joanroche1604@gmail.com>
2024-03-20 17:41:17 +0530

    update hall

--- Backend/Routes/hallRoutes.js
+++ Backend/Routes/hallRoutes.js
@@ -5,11 +5,12 @@ const router=express.Router()
 
 const Hall = require('../Models/hallModel')
 
-const { getAllHalls, createHall } =require('../controllers/hallController')
+const { getAllHalls, createHall ,updateHall} =require('../controllers/hallController')
 
 
 router.get('/', getAllHalls);
 router.post('/add', createHall);
+router.put('/update/:id', updateHall);
 
 
 
