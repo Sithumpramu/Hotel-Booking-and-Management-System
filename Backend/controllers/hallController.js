f1cfc284592aa7ddbc7fe38c35aef04ee0d6a6ba
joanroche444 <joanroche1604@gmail.com>
2024-03-20 17:42:22 +0530

    update

--- Backend/controllers/hallController.js
+++ Backend/controllers/hallController.js
@@ -1,6 +1,6 @@
 // hallController.js
 const { default: mongoose } = require('mongoose')
-const Hall = require('../models/Hall');
+const Hall = require('../Models/hallModel');
 
 // Get all halls
 const getAllHalls = async (req, res) => {
@@ -24,4 +24,33 @@ const createHall = async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 };
-module.exports={ getAllHalls,createHall}
+const updateHall = async (req, res) => {
+    const { id } = req.params;
+    const { Name, description, capacity, price } = req.body;
+
+    try {
+        // Build the update object based on fields that have changed
+        const updateFields = {};
+        if (Name) updateFields.Name = Name;
+        if (description) updateFields.description = description;
+        if (capacity) updateFields.capacity = capacity;
+        if (price) updateFields.price = price;
+
+        // Find and update the hall only if at least one field has changed
+        const hall = await Hall.findByIdAndUpdate(id, { $set: updateFields }, { new: true });
+
+        if (!hall) {
+            return res.status(404).json({ error: 'Hall not found' });
+        }
+
+        res.json(hall);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
+
+
+
+
+module.exports={ getAllHalls,createHall,updateHall}
