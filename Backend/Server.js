825355344061ef8991153f345fbf471c6375ca6b
Mindulee1018 <fonsekamindu@gmail.com>
2024-04-06 21:00:17 +0530

    update server and app file

---
+++ Backend/Server.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors');
+const mongoose = require('mongoose')
+const userRoutes = require('./Routes/user');
+const watersportRoutes = require('./Routes/watersport');
+const WatersportReservationRoutes = require('./Routes/WatersportReservation');
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+
+app.use('/user', userRoutes)
+app.use('/watersport',watersportRoutes)
+app.use('/watersportReservation',WatersportReservationRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT , () => {
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  
+ 
+
+
+
