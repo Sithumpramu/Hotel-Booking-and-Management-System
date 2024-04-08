421459183176b40838866cee6e88a1e5244c3700
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2024-04-08 08:21:20 +0530

    clean

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
-import "./App.css";
+import './App.css';
 
-import Home from "./Pages/Home";
-import Signup from "./Pages/signup";
-import Login from "./components/Login";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import PasswordReset from "./Pages/PasswordReset";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from './Pages/Home';
+import Signup from './Pages/signup';
+import Login from './components/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PasswordReset from './Pages/PasswordReset'
+import {Route, Routes, BrowserRouter} from "react-router-dom";
 import ForgotPassword from "./Pages/ForgotPwd";
-import Watersport from "./Pages/Staff/WatersportManagement";
-import ManagerDashboard from "./Pages/Staff/ManagerDashboard";
-import AddActivity from "./Pages/Staff/AddActivity";
-import WatersportManage from "./Pages/Staff/WatersportManagement";
-import AddReserv from "./Pages/Staff/addWatersportReserv";
-import SelectActivity from "./Pages/Staff/SelectActivity";
-import WatersportReservations from "./Pages/Staff/watersportReservations";
-import ReceptionDashboard from "./Pages/Staff/receptionDashboard";
-
 import AdminDash from "./Pages/Staff/AdminDash";
 import ManagerDash from "./Pages/Staff/ManagerDash";
 import StaffDash from "./Pages/Staff/StaffDash";
@@ -25,7 +16,6 @@ import Staffmanage from "./Pages/Staff/staffManage";
 import Usermanage from "./Pages/Staff/UserManage";
 import AccountManage from "./Pages/Staff/AccountManage";
 import InitialRedirect from "./context/initialDirect";
-
 // Layout component with header and footer
 const Layout = ({ children }) => (
   <div>
@@ -40,30 +30,54 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="" element={<InitialRedirect />} />
-          <Route path="/Dashboard" element={<Layout><Home /></Layout> }/>
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/ForgotPwd" element={<ForgotPassword />} />
-          <Route path="/user/resetPassword/:token" element={<PasswordReset />}/>
-
-          <Route path="/watersport" element={<Watersport />} />
-          <Route path="/ManagerDashboard" element={<ManagerDashboard />} />
-          <Route path="/AddActivity" element={<AddActivity />} />
-          <Route path="/WatersportsManagement" element={<WatersportManage />} />
-          <Route path="/addWatersportsReservation" element={<AddReserv />} />
-          <Route path="/selectActivity" element={<SelectActivity />} />
-          <Route path="/watersportReservations" element={<WatersportReservations />}/>
-
-          
-          <Route path="/ReceptionDashboard" element={<ReceptionDashboard />} />
-          <Route path="/AdminDashbord" element={<AdminDash />} />
-          <Route path="/ManagerDashbord" element={<ManagerDash />} />
-          <Route path="/StaffDashbord" element={<StaffDash />} />
-          <Route path="/Staffmanage" element={<Staffmanage />} />
-          <Route path="/Usermanage" element={<Usermanage />} />
-          <Route path="/Accountmanage" element={<AccountManage />} />
-          
+         <Route
+            path=""
+            element={<InitialRedirect />} 
+          />
+          <Route
+            path="/Dashboard"
+            element={<Layout><Home /></Layout>}
+          />
+          <Route
+            path="/Signup"
+            element={<Signup />}
+          />
+          <Route
+            path="/Login"
+            element={<Login />}
+          />
+          <Route
+            path="/ForgotPwd"
+            element={<ForgotPassword />}
+          />
+          <Route
+            path="/user/resetPassword/:token"
+            element={<PasswordReset />}
+          />
+          <Route
+            path="/AdminDashbord"
+            element={<AdminDash />}
+          />
+          <Route
+            path="/ManagerDashbord"
+            element={<ManagerDash />}
+          />
+          <Route
+            path="/StaffDashbord"
+            element={<StaffDash />}
+          />
+          <Route
+            path="/Staffmanage"
+            element={<Staffmanage/>}
+          />
+          <Route
+            path="/Usermanage"
+            element={<Usermanage/>}
+          />
+           <Route
+            path="/Accountmanage"
+            element={<AccountManage/>}
+          />
         </Routes>
       </BrowserRouter>
     </div>
