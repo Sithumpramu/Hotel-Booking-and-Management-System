5ac363d84eabbd2ec25831125572071a4fd09e04
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2024-04-08 18:38:55 +0530

    manager dashboard design

--- frontend/src/Pages/Staff/ManagerDashboard.js
+++ frontend/src/Pages/Staff/ManagerDashboard.js
@@ -2,28 +2,10 @@ function Dashboard(){
     return(
       <div>
 
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
+        <h1 className="my-5">Welcome to Manager Dashboard!</h1>
 
-        <br></br>
-
-        <h1>Welcome to Manager Dashboard!</h1>
-
-        <br></br>
-
-        <div class="d-grid gap-2 col-3 mx-auto">
+        <hr></hr>
+        <div class="d- justify-content-center align-items-center">
           <button class="btn btn-outline-info" type="button">User Manager</button>
           <button class="btn btn-outline-info" type="button">Room Manager</button>
           <button class="btn btn-outline-info" type="button">Restaurant Manager</button>
