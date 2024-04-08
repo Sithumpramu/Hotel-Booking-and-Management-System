ccc91d21dc647ced6be871ce317b67fe2fdedf51
Mindulee1018 <fonsekamindu@gmail.com>
2024-04-08 12:44:03 +0530

    update app.js page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import Watersport from "./Pages/Staff/WatersportManagement";
+//import Watersport from "./Pages/Staff/WatersportManagement";
 import ManagerDashboard from "./Pages/Staff/ManagerDashboard";
 import AddActivity from "./Pages/Staff/AddActivity";
 import WatersportManage from "./Pages/Staff/WatersportManagement";
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/watersport" element={<Watersport />} />
+          
           <Route path="/ManagerDashboard" element={<ManagerDashboard />} />
           <Route path="/AddActivity" element={<AddActivity />} />
           <Route path="/WatersportsManagement" element={<WatersportManage />} />
