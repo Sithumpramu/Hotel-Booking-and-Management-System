bcb8ba915577307e6ee9d8de1df12cc95067c1fe
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2024-04-08 19:54:53 +0530

    design

--- frontend/src/Pages/Staff/ManagerDashboard.js
+++ frontend/src/Pages/Staff/ManagerDashboard.js
@@ -1,16 +1,25 @@
+import { useLogout } from '../../hooks/useLogout';
+
 function Dashboard(){
+  const {logout}=useLogout()
+
+  const handlelogout = ()=>{
+    logout()
+  }
     return(
       <div>
-
-        <h1 className="my-5">Welcome to Manager Dashboard!</h1>
-
-        <hr></hr>
-        <div class="d- justify-content-center align-items-center">
-          <button class="btn btn-outline-info" type="button">User Manager</button>
-          <button class="btn btn-outline-info" type="button">Room Manager</button>
-          <button class="btn btn-outline-info" type="button">Restaurant Manager</button>
-          <button class="btn btn-outline-info" type="button">Event & Activity manager</button>
-          <button class="btn btn-outline-info" type="button">Financial Manager</button>
+        <div className="border border-3 pb-3" style={{backgroundColor:" #f8f9fa"}}>        
+        <h3 className="my-5">Welcome to Manager Dashboard!</h3>
+        <div className=""><a href ="" id="userStatus" className="btn" sty onClick={handlelogout} style={{backgroundColor:"rgb(85, 180, 254)", position:"relative", top:"0px"}}>Logout</a></div>
+      
+        </div>
+        <div class="d-grid gap-2 col-3 mx-auto" style={{position:"relative" ,top:"140px"}}>
+          <a href="" className="btn btn-outline-primary mb-3 lh-lg" type="button">Room Manager</a>
+          <a href="" className="btn btn-outline-primary mb-3 lh-lg" type="button">Restaurant Manager</a>
+          <a href="" className="btn btn-outline-primary mb-3 lh-lg" type="button">Event & Activity manager</a>
+        </div>
+        <div>
+       
         </div>
       </div>
       
