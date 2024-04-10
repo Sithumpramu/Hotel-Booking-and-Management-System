60d89e2ea8f139056ce14b3c2b775690f802625f
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2024-04-10 21:23:33 +0530

    done

--- frontend/src/Pages/TableReservation.js
+++ frontend/src/Pages/TableReservation.js
@@ -16,28 +16,25 @@ const TableReservation = () => {
         return <div>Error: {error}</div>;
     }
 
-
-
     return (
-
-        <div className=" vh-100">
+        <div className="vh-100">
             <h1>
                 <u>My Reservations</u>
             </h1>
 
             <div className="">
-                {TableList.map((Table) => {
-                    <div>
-                    <h4>{Table.Date}</h4>
-                    <p><strong>Name: </strong>{Table.Name}</p>
-                    <p><strong>Capacity: </strong>{Table.Capacity}</p>
-                    <p><strong>Email: </strong>{Table.email}</p>
-                    <p><strong>Telephone Number: </strong>{Table.ContactNumber}</p>
+                {TableList.map((Table) => (
+                    <div key={Table._id}>
+                        <h4>{Table.Date}</h4>
+                        <p><strong>Name: </strong>{Table.Name}</p>
+                        <p><strong>Capacity: </strong>{Table.Capacity}</p>
+                        <p><strong>Email: </strong>{Table.email}</p>
+                        <p><strong>Telephone Number: </strong>{Table.contactNumber}</p>
                     </div>
-                })}
+                ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default TableReservation
+export default TableReservation;
