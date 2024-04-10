c5c8c5204e5d3a2e2761bcc84627dc1f2f7838f3
Mindulee1018 <fonsekamindu@gmail.com>
2024-04-11 01:43:57 +0530

    get room Image file

--- Backend/controllers/roomController.js
+++ Backend/controllers/roomController.js
@@ -1,44 +1,38 @@
-const { default: mongoose } = require('mongoose');
-const room = require('../Models/roomModel');
-
+const { default: mongoose } = require("mongoose");
+const room = require("../Models/roomModel");
 
 //get all rooms
 const getRoom = async (req, res) => {
-
   try {
-    const rooms = await room.find({})//get all data
+    const rooms = await room.find({}); //get all data
 
-    res.status(200).json(rooms)//send to browser
-  }
-  catch (error) {
+    res.status(200).json(rooms); //send to browser
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
-
-}
+};
 
 //get single room
 const getsingleRoom = async (req, res) => {
-
-  const { id } = req.params
+  const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    res.status(404).json({ error: 'invalid id' })
+    res.status(404).json({ error: "invalid id" });
   }
 
-  const rooms = await room.findById(id)
+  const rooms = await room.findById(id);
 
   if (!rooms) {
-    res.status(404).json({ error: 'No such room' })
+    res.status(404).json({ error: "No such room" });
   }
 
-  res.status(200).json(rooms)
-}
-
-
+  res.status(200).json(rooms);
+};
 
 //Add a room
 const roomAdd = async (req, res) => {
-  const { Rid, Rtype, description, capacity, NoOfBeds, price, status } = req.body; // Correct the variable name 'status' to 'Status'
+  const { Rid, Rtype, description, capacity, NoOfBeds, price, status } =
+    req.body; // Correct the variable name 'status' to 'Status'
   let imageData = {};
 
   console.log(req.file, "fileee");
@@ -75,7 +69,7 @@ const deleteRoom = async (req, res) => {
   const { id } = req.params;
 
   if (!id) {
-    return res.status(400).json({ error: 'Invalid id' });
+    return res.status(400).json({ error: "Invalid id" });
   }
 
   try {
@@ -83,31 +77,32 @@ const deleteRoom = async (req, res) => {
     const rooms = await room.findOneAndDelete(id);
 
     if (!rooms) {
-      return res.status(404).json({ error: 'No such room' });
+      return res.status(404).json({ error: "No such room" });
     }
 
-    return res.status(200).json({ status: 'room deleted' });
+    return res.status(200).json({ status: "room deleted" });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
 //update a room
 const updateRoom = async (req, res) => {
   const { id } = req.params;
-  const { Rid, Rtype, description, capacity, NoOfBeds, price, status } = req.body;
+  const { Rid, Rtype, description, capacity, NoOfBeds, price, status } =
+    req.body;
 
   try {
     // Validate if the provided ID is a valid MongoDB ObjectId
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ error: 'Invalid Room ID' });
+      return res.status(400).json({ error: "Invalid Room ID" });
     }
 
     // Check if the room exists in the database
     const rooms = await room.findById(id);
     if (!rooms) {
-      return res.status(404).json({ error: 'Room not found' });
+      return res.status(404).json({ error: "Room not found" });
     }
 
     // Build the update object based on fields that have changed
@@ -120,17 +115,20 @@ const updateRoom = async (req, res) => {
     if (price) updateFields.price = price;
     if (status) updateFields.status = status;
 
-
     // Update the room only if at least one field has changed
     if (Object.keys(updateFields).length > 0) {
-      const rooms = await room.findByIdAndUpdate(id, { $set: updateFields }, { new: true });
+      const rooms = await room.findByIdAndUpdate(
+        id,
+        { $set: updateFields },
+        { new: true }
+      );
       res.json(rooms);
     } else {
-      res.json({ message: 'No changes detected' });
+      res.json({ message: "No changes detected" });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
--- frontend/src/Pages/rooms.js
+++ frontend/src/Pages/rooms.js
@@ -1,37 +1,47 @@
-
 import React, { useState } from "react";
 import RoomList from "../hooks/useRoomList";
 
-
 function Room() {
-    const { rooms } = RoomList();
+  const { rooms } = RoomList();
 
-    return (
-        <div>
-            <div className='row bs' >
-                {rooms.map((rooms) => (
-                    <div className="col-md-7">
-                        <div className="card">
-                            <div className="card-body">
-                                <p className="card-text">{rooms.Rid}</p>
-                                <p className="card-text">{rooms.Rtype}</p>
-                                <p className="card-text">{rooms.description}</p>
-                                <p className="card-text">{rooms.capacity}</p>
-                                <p className="card-text">{rooms.NoOfBeds}</p>
-                                <p className="card-text">{rooms.price}</p>
-                                <p className="card-text">{rooms.status}</p>
+  return (
+    <div>
+      <div className="row bs">
+        {rooms.map((rooms) => (
+          <div className="col-md-7">
+            <div className="card">
+              <div className="card-body">
+                {rooms.Image && rooms.Image.data && (
+                  <img
+                    style={{ width: "10rem" }}
+                    src={`data:${rooms.Image.contentType};base64,${btoa(
+                      String.fromCharCode.apply(null, rooms.Image.data.data)
+                    )}`}
+                    className="card-img-top mb-1"
+                    alt={rooms.Rtype}
+                  />
+                )}
 
-                                <div style={{ float: 'right' }}>
-                                    <a href="/CustomerDetails" className="btn btn-info">Book Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                <p className="card-text">{rooms.Rid}</p>
+                <p className="card-text">{rooms.Rtype}</p>
+                <p className="card-text">{rooms.description}</p>
+                <p className="card-text">{rooms.capacity}</p>
+                <p className="card-text">{rooms.NoOfBeds}</p>
+                <p className="card-text">{rooms.price}</p>
+                <p className="card-text">{rooms.status}</p>
+
+                <div style={{ float: "right" }}>
+                  <a href="/CustomerDetails" className="btn btn-info">
+                    Book Now
+                  </a>
+                </div>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default Room;
