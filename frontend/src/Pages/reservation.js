00354bb42dc3abf8f55bbdb615d2eba6a4c02aee
Imzi123 <imayaperera1@gmail.com>
2024-04-11 12:05:44 +0530

    Aluth ewa

--- frontend/src/Pages/reservation.js
+++ frontend/src/Pages/reservation.js
@@ -1,59 +1,63 @@
-import { useEffect, useState } from "react"
+import { useState } from "react";
+import React from 'react';
+import useAddRoomReserve from "../hooks/useAddRoomReserve";
 
-const ReservationList = () => {
-  const [room, setReservation] = useState(null);
-
-  useEffect(() => {
-    const room = async () => {
-      try {
-        const response = await fetch("http://localhost:4000//");
-        if (!response.ok) {
-          throw new Error("Failed to fetch rooms");
-        }
-        const json = await response.json();
-        setReservation(json);
-      } catch (error) {
-        console.error("Error fetching rooms data:", error);
-      }
-    };
-    setReservation();
-  }, []);
+const AddNewRoomReserve = () => {
+  const [Checkindate, setCheckindate] = useState("");
+  const [Checkoutdate, setCheckoutdate] = useState("");
+  const [Rtype, setRtype] = useState("");
+  const [NoOfGuests, setNoOfGuests] = useState("");
 
 
 
-  function Rooms() {
-    const [checkInDate, setCheckInDate] = useState('');
-    const [checkOutDate, setCheckOutDate] = useState('');
-    const [numberOfPeople, setNumberOfPeople] = useState('');
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+  const { addRoomReserve, isLoading, error } = useAddRoomReserve();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await addRoomReserve(Checkindate, Checkoutdate, Rtype, NoOfGuests);
+  };
+
+  function validation() {
+    var submit = document.getElementById("submit");
+
+    if (Checkindate === "" && Checkoutdate === "" && Rtype === "" && NoOfGuests === "") {
+      document.getElementById("Error").innerHTML = "All fields must be filled.";
     }
-
-    return (
-      <div className="background vh-100 d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url("katha.jpg")' }}>
-        <div className="card">
-
-          <form onSubmit={handleSubmit}>
-            <label>
-              Check-in Date:
-              <input type="date" value={checkInDate} onChange={(e) => setCheckInDate(e.target.value)} />
-            </label>
-            <label>
-              Check-out Date:
-              <input type="date" value={checkOutDate} onChange={(e) => setCheckOutDate(e.target.value)} />
-            </label>
-            <label>
-              Number of People:
-              <input type="number" value={numberOfPeople} onChange={(e) => setNumberOfPeople(e.target.value)} />
-            </label>
-
-          </form>
-          <button type="submit">Book Now</button>
-        </div>
-      </div>
-    )
   }
+
+
+  return (
+    <div className="background vh-100 d-flex justify-content-center align-items-center" style={{ backgroundImage: 'url("katha.jpg")' }}>
+      <div className="card">
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Check-in Date:
+          </label>
+          <input type="date" value={Checkindate} onChange={(e) => setCheckindate(e.target.value)} />
+
+          <label>
+            Check-out Date:
+          </label>
+          <input type="date" value={Checkoutdate} onChange={(e) => setCheckoutdate(e.target.value)} />
+
+          <label>
+            Room Type:
+          </label>
+          <input type="date" value={Rtype} onChange={(e) => setRtype(e.target.value)} />
+
+          <label>
+            Number of People:
+          </label>
+          <input type="number" value={Rtype} onChange={(e) => setRtype(e.target.value)} />
+
+        </form>
+        <button type="submit">Book Now</button>
+      </div>
+    </div>
+  )
 }
-export default ReservationList;
+
+export default AddNewRoomReserve;
